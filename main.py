3be2c91e018d69ef4a9afbb5506b0471c35ab445
Aster-orion-dev <2664856683@qq.com>
2026-08-06 20:43:23 +0800

    Change TARGET_TIME and ENDTIME environment variables

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ get_current_dayofweek = lambda action: (
 
 SLEEPTIME = 0.3
 
-TARGET_TIME = os.getenv("TARGET_TIME", "17:00:00")
-ENDTIME = os.getenv("ENDTIME", "17:01:20")
+TARGET_TIME = os.getenv("TARGET_TIME", "08:00:00")
+ENDTIME = os.getenv("ENDTIME", "08:01:20")
 
 
 ENABLE_SLIDER = False
