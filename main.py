cd5520543b89259cdad4bcc3459c68aaf0a9cf0e
Aster-orion-dev <2664856683@qq.com>
2026-08-08 00:03:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ get_current_dayofweek = lambda action: (
 
 SLEEPTIME = 0.3
 
-TARGET_TIME = os.getenv("TARGET_TIME", "18:00:00")
-ENDTIME = os.getenv("ENDTIME", "18:01:20")
+TARGET_TIME = os.getenv("TARGET_TIME", "08:00:00")
+ENDTIME = os.getenv("ENDTIME", "08:01:20")
 
 
 ENABLE_SLIDER = False
